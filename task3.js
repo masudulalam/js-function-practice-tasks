34535f124ba52d84644fde0190d9d43845291f52
masudulalam <masuudulalam@gmail.com>
2024-03-21 01:36:00 +0600

    task 3 solved

--- task3.js
+++ task3.js
@@ -7,3 +7,17 @@ Write a function called make_avg() which will take an array of integers and the 
 
 
 // *Task-3 solution:
+
+function makeAvg (arr, size) {
+    let sum = 0;
+    for(let i = 0; i < size; i++) {
+        sum = sum + arr[i];
+    } 
+
+    return sum / size;
+}
+
+const array = [1, 2, 3, 4, 5];
+const size = array.length;
+const average = makeAvg(array, size);
+console.log('Average:', average);

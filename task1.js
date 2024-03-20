4ad8659d08adf656d58ecb8f37491e05ebba9cbc
masudulalam <masuudulalam@gmail.com>
2024-03-21 01:40:02 +0600

    update and task 2 solved

--- task1.js
+++ task1.js
@@ -15,7 +15,6 @@ function multiplyFourNumbers (a, b, c, d) {
 const n1 = 1;
 const n2 = 2;
 const n3 = 3;
-
 const n4 = 4;
 const output = multiplyFourNumbers(n1, n2, n3, n4);
 console.log('Result:', output);
